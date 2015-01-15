b29c5aafe9cb7262c1b361afc075edd84a0c4194
dusanders <dusanders@gmail.com>
2015-01-14 20:03:26 -0600

    update

--- C/Androidscript.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('#background').fadeIn(2000, function () {
-    });
-    $('#textBackground').slideDown("slow", function () {
-        $('.content-div').slideDown("slow");
-    });
-});
